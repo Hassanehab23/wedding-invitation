6e6e84bdf0a0ba831649cf138f278b129d96bc46
Hassan <Hsnqtb42@gmsil.com>
2026-07-12 19:38:38 +0300

    Update music playlist and wedding invitation

--- src/Components/SecondHeroComponent/SecondHero.tsx
+++ src/Components/SecondHeroComponent/SecondHero.tsx
@@ -1,68 +1,30 @@
 import { motion } from "framer-motion";
-import weddingImg from "../../assets/Images/Wedding2.jpg";
-
+import weddingImg from "../../assets/Images/SecondHeroImg.png";
 
 export default function SecondHero() {
- return (
+  return (
     <>
-
       {/* HERO SECTION */}
-      <section id="SecondHero" className="relative h-screen overflow-hidden">
+      <section id="SecondHero" className="relative w-full h-screen overflow-hidden flex items-center justify-center">
+        
+        {/* Background Image Container */}
+        <div className="absolute inset-0 w-full h-full -z-10">
+          <motion.img
+            src={weddingImg}
+            alt="Wedding Background"
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
 
+        {/* Overlay: طبقة شفافة سوداء */}
+        <div className="absolute inset-0 bg-black/30 pointer-events-none" />
 
-        {/* Background Image */}
-              {/* الصورة */}
-              <div
-              className="
-              w-full
-              aspect-4/3
-              bg-black
-              overflow-hidden
-              "
-              >
+        {/* المحتوى (نصوص أو أزرار) يوضع هنا ويكون فوق الصورة */}
+        <div className="relative z-10 text-center">
+          
+        </div>
 
-                <motion.img
-
-                  src={weddingImg}
-
-                  alt={weddingImg}
-
-                  whileHover={{
-                    scale:1.05
-                  }}
-
-                  transition={{
-                    duration:.6
-                  }}
-
-                  className="
-                  w-full
-                  h-full
-                  object-contain
-                  "
-                />
-
-              </div>
-
-
-
-        {/* Overlay */}
-        <div 
-          className="
-          absolute 
-          inset-0 
-          bg-black/5
-          "
-        />
       </section>
-
-
-
-
-
-
-
-
     </>
   );
 }
